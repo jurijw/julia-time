0cd882708ffcf100c5877bc99c96fad327a603c5
Jurij Waite <jurijlwaite@gmail.com>
2021-05-26 00:45:47 +0200

    Added homework 1.

---
+++ notebook0.jl
@@ -0,0 +1,31 @@
+### A Pluto.jl notebook ###
+# v0.14.6
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b2c2fbd6-bd94-11eb-0a3b-8b19749a386f
+begin
+	x = 10
+	
+	range = (1:19)
+	
+	print(range)
+end
+
+# ╔═╡ f9476e18-5c8f-4e2c-8eec-479c5a4b0aab
+range
+
+# ╔═╡ 25074e5a-2b56-4d9c-8ddb-5a7d89f5bdd5
+for i in range
+	print(i)
+end
+
+# ╔═╡ 120b6dbe-830c-4371-8127-7b47ae041871
+
+
+# ╔═╡ Cell order:
+# ╠═b2c2fbd6-bd94-11eb-0a3b-8b19749a386f
+# ╠═f9476e18-5c8f-4e2c-8eec-479c5a4b0aab
+# ╠═25074e5a-2b56-4d9c-8ddb-5a7d89f5bdd5
+# ╠═120b6dbe-830c-4371-8127-7b47ae041871
